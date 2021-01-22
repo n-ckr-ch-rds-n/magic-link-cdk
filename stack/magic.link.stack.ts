ba5f741ad38b1acf8246bd0e5ee2f9c2adb266ba
nick_richardson <nick.richardson@rezonence.com>
2021-01-22 15:56:13 +0000

    define login lambda

--- stack/magic.link.stack.ts
+++ stack/magic.link.stack.ts
@@ -1,7 +1,16 @@
 import {Stack, App, StackProps} from "@aws-cdk/core";
+import {AssetCode, Runtime, Function} from "@aws-cdk/aws-lambda";
 
 export class MagicLinkStack extends Stack {
     constructor(app: App, id: string, props?: StackProps) {
         super(app, id, props);
+
+        const login = new Function(this, "Login", {
+            runtime: Runtime.NODEJS_12_X,
+            handler: "login.handler",
+            code: new AssetCode("api")
+        })
+
     }
+
 }
