04a5cdda3ec27ff62cd6ea1d3e140ed73deba061
nick_richardson <nick.richardson@rezonence.com>
2021-01-22 16:08:25 +0000

    add api event source to login function

--- stack/magic.link.stack.ts
+++ stack/magic.link.stack.ts
@@ -1,5 +1,6 @@
 import {Stack, App, StackProps} from "@aws-cdk/core";
 import {AssetCode, Runtime, Function} from "@aws-cdk/aws-lambda";
+import {ApiEventSource} from "@aws-cdk/aws-lambda-event-sources";
 
 export class MagicLinkStack extends Stack {
     constructor(app: App, id: string, props?: StackProps) {
@@ -9,7 +10,7 @@ export class MagicLinkStack extends Stack {
             runtime: Runtime.NODEJS_12_X,
             handler: "login.handler",
             code: new AssetCode("api")
-        })
+        }).addEventSource(new ApiEventSource("post", "/login")) // enable CORS?
 
     }
 
